ab0f96c84f6aeb649e717ce8f07c653b3ebc17cc
ta <ntparis9@gmail.com>
2021-08-05 15:42:58 +0200

    page-produit okk

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ let container = document.createElement('div');
 container.className = "container_item";
 document.querySelector('.affichang_products').appendChild(container);
 let teddies__data = [];
-const images_products = document.createElement('div');
-images_products.className = "image_product";
-document.querySelector('.container_item').appendChild(images_products);
-const description_products = document.createElement('div');
-description_products.className = "description";
-document.querySelector('.container_item').appendChild(description_products);
-const btn_select = document.createElement('div');
-btn_select.className = "btn_select";
-document.querySelector('.container_item').appendChild(btn_select);
+// const images_products = document.createElement('div');
+// images_products.className = "image_product";
+// document.querySelector('.container_item').appendChild(images_products);
+// const description_products = document.createElement('div');
+// description_products.className = "description";
+// document.querySelector('.container_item').appendChild(description_products);
+// const btn_select = document.createElement('div');
+// btn_select.className = "btn_select";
+// document.querySelector('.container_item').appendChild(btn_select);
 
 
 
@@ -36,7 +36,7 @@ function afichage_teddies(data) {
     let images = '';
     let p = '';
     let affisher = '';
-
+    let item_container = '';
 
 
     data.forEach(function (item) {
@@ -44,19 +44,27 @@ function afichage_teddies(data) {
         let description = item.description;
         let image = item.imageUrl;
 
-        affisher += '<button class="btn" type="submit">add-panier</button><button class="btn" type="submit">favorite</button>'
+        affisher = '<button class="btn" type="submit">add-panier</button><button class="btn" type="submit">favorite</button>'
 
-        console.log('item: ', item.imageUrl);
-        images += `<a href="${image}" target="_black"><img src="${image}"></a>`
 
-        p += `<div><strong>description:</strong>${description}<br>colors: <br> price:</div>`
+        images = `<a href="${image}" target="_black"><img src="${image}"></a>`
+
+        p = `<div><strong>description:</strong>${description}<br>colors: <br> price:</div>`
+        item_container += `<div>${images}${p}${affisher}</div>`;
+
+
+
+
+
     });
+    console.log('item_container: ', item_container);
+    document.querySelector('.container_item').innerHTML = item_container;
 
 
 
-    document.querySelector('.image_product').innerHTML = images;
-    document.querySelector('.description').innerHTML = p;
-    document.querySelector('.btn_select').innerHTML = affisher;
+    // document.querySelector('.image_product').innerHTML = images;
+    // document.querySelector('.description').innerHTML = p;
+    // document.querySelector('.btn_select').innerHTML = affisher;
 
 
 
